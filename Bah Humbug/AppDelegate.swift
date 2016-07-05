3f5023bfee25de1d6c59d6dbc33cd1f9b9a134df
Hugh Rawlinson <hugh@rawlinson.com>
2016-07-05 13:56:46 +0200

    Initial Commit

---
+++ Bah Humbug/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bah Humbug
+//
+//  Created by Hugh Rawlinson on 05/07/16.
+//  Copyright © 2016 Hugh Rawlinson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bah Humbug/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bah Humbug
+//
+//  Created by Hugh Rawlinson on 05/07/16.
+//  Copyright © 2016 Hugh Rawlinson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

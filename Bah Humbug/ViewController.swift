0b1f49c59d27bed972208f9557ac77402d4805ab
Hugh Rawlinson <hugh@rawlinson.com>
2016-07-05 18:46:09 +0200

    Initial Commit

--- Bah Humbug/ViewController.swift
+++ Bah Humbug/ViewController.swift
@@ -7,21 +7,45 @@
 //
 
 import Cocoa
+import AVFoundation
+import AudioKit
 
 class ViewController: NSViewController {
+    @IBOutlet weak var freqSlider :NSSlider!
+    @IBOutlet weak var carrierScalarSlider :NSSlider!
+    @IBOutlet weak var muteButton :NSButton!
+    let osc = AKFMOscillator()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        AudioKit.output = osc
+        AudioKit.start()
+        osc.start()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector:(#selector(NSWindowDelegate.windowWillClose(_:))),
+                                                         name:NSWindowWillCloseNotification,
+                                                         object:self.view.window)
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    @IBAction func sliderUpdated(sender: AnyObject) {
+        osc.baseFrequency = baseFreq(freqSlider.intValue)
+        osc.carrierMultiplier = carrierScalarSlider.doubleValue
     }
-
-
+    
+    func windowWillClose(notification: NSNotification) {
+        NSApplication.sharedApplication().terminate(0)
+    }
+    
+    func baseFreq(sliderVal: Int32) -> Double {
+        return Int(sliderVal).midiNoteToFrequency()
+    }
+    
+    @IBAction func toggleAudio(sender: AnyObject) {
+        //renderingAudio = !renderingAudio
+        muteButton.doubleValue > 0 ? osc.start() : osc.stop()
+    }
 }
 
